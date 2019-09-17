3030bacea15ea1b1dade222e2cae08469c97f7ab
Yari Andres <yari.andres@gmail.com>
2019-09-18 01:17:23 +0200

    file update

--- book-store-server/server.js
+++ book-store-server/server.js
@@ -1,13 +1,15 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const bookRouter = require("./services/books");
+const cors = require("cors");
 
 const server = express();
 
+server.use(cors());
 server.use(bodyParser.json());
 
-server.use("/books", bookRouter)
+server.use("/books", bookRouter);
 
 server.listen(3450, () => {
   console.log("SERVER IS RUNNING ON 3450");
-})
+});
--- book-store/src/App.js
+++ book-store/src/App.js
@@ -2,10 +2,19 @@ import React from 'react';
 import './App.css';
 
 class App extends React.Component {
-  state = {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      comments: [],
+      nComments: 0,
+      asin: ""
+    };
   }
 
   render() {
+
+    var toMap = 
     return (
       <div className="App">
         <header className="App-header">
@@ -16,7 +25,12 @@ class App extends React.Component {
   }
 
   componentDidMount = async () => {
+    var res = await fetch("http://localhost:3450/books?category" + this.props.genre);
+    var books = await res.json();
 
+    this.setState({
+      books: books
+    })
   }
 
 }
