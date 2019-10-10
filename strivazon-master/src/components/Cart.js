a6f16320244cb695ac57542afe33005711d2f37c
Yari Andres <yari.andres@gmail.com>
2019-10-10 09:37:37 +0200

    adding products to cart

--- strivazon-master/src/components/Cart.js
+++ strivazon-master/src/components/Cart.js
@@ -4,16 +4,20 @@ import Button from "react-bootstrap/Button";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faTrash } from "@fortawesome/free-solid-svg-icons";
 
+import { connect } from "react-redux";
+
+const mapStateToProps = state => state;
+
 class Cart extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      cart: []
-    };
-  }
+  // constructor(props) {
+  //   super(props);
+  //   this.state = {
+  //     cart: []
+  //   };
+  // }
 
   render() {
-    const cart = this.state.cart.map(bookId =>
+    const cart = this.props.cart.products.map(bookId =>
       books.find(book => book.id === bookId)
     );
 
@@ -22,7 +26,7 @@ class Cart extends Component {
         <ul className="col-sm-12" style={{ listStyle: "none" }}>
           {cart.map((book, i) => (
             <li key={i} className="my-4">
-              <Button variant="danger" onClick={() => {}}>
+              <Button variant="danger" onClick={() => { }}>
                 <FontAwesomeIcon icon={faTrash} id="trashIcon" />
               </Button>
               <img
@@ -48,4 +52,4 @@ class Cart extends Component {
   }
 }
 
-export default Cart;
+export default connect(mapStateToProps)(Cart);
