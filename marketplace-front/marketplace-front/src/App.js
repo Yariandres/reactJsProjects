06db8bacd9a3e9386cdc2a8e0654ad65ecc4917a
Yari Andres <yari.andres@gmail.com>
2019-11-24 17:55:19 +0100

    editing files

--- marketplace-front/marketplace-front/src/App.js
+++ marketplace-front/marketplace-front/src/App.js
@@ -5,7 +5,6 @@ class App extends React.Component {
 
   state = {
     products: [],
-    asin: "",
     title: "",
     img: "",
     price: 0,
@@ -14,7 +13,6 @@ class App extends React.Component {
 
   createProduct = async () => {
     var toSend = {
-      asin: this.state.asin,
       title: this.state.title,
       img: this.state.img,
       price: this.state.price
@@ -34,10 +32,10 @@ class App extends React.Component {
       <div className="App">
         <header className="App-header">
 
-          <input type="number" value={this.state.asin} onChange={val => this.setState({ asin: val.target.value })} placeholder="ASIN " />
           <input type="text" value={this.state.title} onChange={val => this.setState({ title: val.target.value })} placeholder="Product title" />
           <input type="text" value={this.state.img} onChange={val => this.setState({ img: val.target.value })} placeholder="img url" />
-          <input type="text" value={this.state.price} onChange={val => this.setState({ price: val.target.value })} placeholder="Product price" />
+          <input type="text" value={this.state.price} onChange={val => this.setState({ price: val.target.value })} placeholder="price" />
+
           <input type="button" onClick={() => this.createProduct()} value="Create Product" />
 
           {this.state.products.map(product => <div key={product.id}>
@@ -45,7 +43,6 @@ class App extends React.Component {
             <img src={product.img} />
             <p>Price: ${product.price}</p>
             <p>Date Created: {product.createdAt}</p>
-            <p>ASIN #: {product.asin}</p>
             <hr />
           </div>)}
         </header>
